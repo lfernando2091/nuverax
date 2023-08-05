b144854755593d112cda44ba931efb66ea33b936
Luis Hernandez <lfernando2091@gmail.com>
2023-08-05 14:10:01 -0600

    WORKING

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import './App.css';
-import {BrowserRouter} from "react-router-dom";
-import {PagesRouter} from "./pages/router";
+import {AppState, AppContext} from "./@core";
+import {RouterProvider} from "react-router-dom";
+import {router} from "./pages/router";
 
 function App() {
+    const state: AppState = {
+        appVersion: "0.0.1"
+    }
+
   return (
-      <BrowserRouter>
-        <PagesRouter/>
-      </BrowserRouter>
-  );
+      <AppContext.Provider value={state}>
+          <RouterProvider router={router} />
+      </AppContext.Provider>
+  )
 }
 
 export default App;
--- src/pages/router.tsx
+++ src/pages/router.tsx
@@ -1,23 +1,16 @@
-import {AppState, AppContext} from "../@core";
-import {Route, Routes} from "react-router-dom";
+import {createBrowserRouter, createRoutesFromElements, Route} from "react-router-dom";
 import {SpaceLayout, spaceRouter} from "./space/SpaceLayout";
 import {PagesLayout, pagesRouter} from "./PagesLayout";
 
-export const PagesRouter = () => {
-    const state: AppState = {
-        appVersion: "0.0.1"
-    }
-
-    return (
-        <AppContext.Provider value={state}>
-            <Routes>
-                <Route path="/" element={<PagesLayout />}>
-                    { pagesRouter() }
-                </Route>
-                <Route path="/s/:id" element={<SpaceLayout />}>
-                    { spaceRouter() }
-                </Route>
-            </Routes>
-        </AppContext.Provider>
+export const router = createBrowserRouter(
+    createRoutesFromElements(
+        <>
+            <Route path="/" element={<PagesLayout />}>
+                { pagesRouter() }
+            </Route>
+            <Route path="/s/:id" element={<SpaceLayout />}>
+                { spaceRouter() }
+            </Route>
+        </>
     )
-}
+)
