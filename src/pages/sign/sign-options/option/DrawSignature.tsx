2e7599e93cc74f2cd1979b3300c1f4099c3cc301
Luis Hernandez <lfernando2091@gmail.com>
2023-09-01 11:05:39 -0600

    working

--- src/pages/sign/sign-options/option/DrawSignature.tsx
+++ src/pages/sign/sign-options/option/DrawSignature.tsx
@@ -1,8 +1,11 @@
-import {Box, Button, Typography} from "@mui/material";
+import {Box, Button, Grid, ToggleButton, ToggleButtonGroup, Typography} from "@mui/material";
 import React, {useEffect, useRef, useState} from "react";
 import {Layer, Stage, Line } from "react-konva";
 import Konva from "konva"
 import { If } from "../../../../components/common/IfStatement";
+import CreateIcon from '@mui/icons-material/Create';
+import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
+import {useTranslation} from "react-i18next";
 
 type LineDef = {
     points: number[]
@@ -14,7 +17,8 @@ type DrawAreaProps = {
     save: boolean
     onSave: (raw: Blob) => void
     onClearLines?: () => void
-    clearLines?: boolean
+    clearLines?: boolean,
+    pencilType: string
 }
 
 const DrawArea = ({
@@ -22,7 +26,8 @@ const DrawArea = ({
                       save,
                       onSave,
                       clearLines,
-                      onClearLines
+                      onClearLines,
+                      pencilType
 }: DrawAreaProps) => {
     const [lines, setLines] = useState<LineDef[]>([])
     const isDrawing = useRef(false)
@@ -59,7 +64,7 @@ const DrawArea = ({
         if (e.target.getStage() !== null) {
             const pos = e.target.getStage()?.getPointerPosition();
             if (pos) {
-                setLines([...lines, { points: [pos.x, pos.y] }]);
+                setLines([...lines, { points: [pos.x, pos.y], tool: pencilType }]);
             }
         }
     }
@@ -107,7 +112,7 @@ const DrawArea = ({
                         key={i}
                         points={line.points}
                         stroke="#292929"
-                        strokeWidth={5}
+                        strokeWidth={6}
                         tension={0.5}
                         lineCap="round"
                         globalCompositeOperation={
@@ -121,9 +126,12 @@ const DrawArea = ({
 }
 
 export const DrawSignature = () => {
+    const { t } = useTranslation("signNS");
     const refDiv = useRef<HTMLDivElement>(null)
     const [ready, setReady] = useState(false)
     const [save, setSave] = useState(false)
+    const [clear, setClear] = useState(false)
+    const [pencilType, setPencilType] = useState("pencil")
 
     useEffect(() => {
         if (refDiv.current) {
@@ -132,31 +140,71 @@ export const DrawSignature = () => {
     }, [refDiv])
 
     const onSaveData = (value: Blob) => {
-
+        setSave(false)
     }
 
+    const onClickClear = () => {
+        setClear(true)
+    }
+
+    const onClickSave = () => {
+        setSave(true)
+    }
+
+    const onClearLines = () => {
+        setClear(false)
+    }
+
+    const handlePencil = (
+        event: React.MouseEvent<HTMLElement>,
+        newValue: string | null
+    ) => {
+        if (newValue !== null) {
+            setPencilType(newValue)
+        }
+    }
     return (<>
-        <Typography variant="h6" gutterBottom>
-            h6. Heading
+        <Typography sx={{ marginTop: "10px" }} variant="h6" gutterBottom>
+            {t("drawSignatureTxt")}
         </Typography>
         <Typography variant="subtitle1" gutterBottom>
-            subtitle1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
-            blanditiis tenetur
+            {t("drawSignatureDescTxt")}
         </Typography>
-        <Box ref={refDiv} sx={{
-            height: "300px",
-            bgcolor: "#fff",
-            border: "2px solid aqua"
-        }}>
-            <If condition={refDiv.current !== null && ready}>
-                <DrawArea onSave={onSaveData} save={save} parent={refDiv.current!!}/>
-            </If>
-        </Box>
-        <Button variant="text">
-            Borrar todo
-        </Button>
-        <Button variant="text">
-            Guardar
-        </Button>
+        <Grid container>
+            <Grid item xs={12} sm={12} md={2} lg={1} xl={4}></Grid>
+            <Grid item xs={12} sm={12} md={8} lg={10} xl={4}>
+                <ToggleButtonGroup size="small" onChange={handlePencil} value={pencilType} exclusive>
+                    <ToggleButton value="eraser">
+                        <DeleteOutlineIcon fontSize="small"/>
+                    </ToggleButton>
+                    <ToggleButton value="pencil">
+                        <CreateIcon fontSize="small"/>
+                    </ToggleButton>
+                </ToggleButtonGroup>
+                <Button onClick={onClickClear} variant="text">
+                    {t("clearTxt")}
+                </Button>
+                <Box ref={refDiv} sx={{
+                    height: "300px",
+                    bgcolor: "#fff",
+                    border: "2px solid aqua",
+                    cursor: "crosshair"
+                }}>
+                    <If condition={refDiv.current !== null && ready}>
+                        <DrawArea
+                            clearLines={clear}
+                            onClearLines={onClearLines}
+                            onSave={onSaveData}
+                            save={save}
+                            pencilType={pencilType}
+                            parent={refDiv.current!!}/>
+                    </If>
+                </Box>
+                <Button onClick={onClickSave} sx={{ marginTop: "10px" }} fullWidth variant="contained">
+                    {t("acceptTxt")}
+                </Button>
+            </Grid>
+            <Grid item xs={12} sm={12} md={2} lg={1} xl={4}></Grid>
+        </Grid>
     </>)
 }
